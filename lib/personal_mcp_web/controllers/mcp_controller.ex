cbe899f91038cd47bc3918085e86fe277add1266
Charlie Anderson <charliepjanderson@gmail.com>
2026-08-21 10:17:11 +0000

    Adds heartbeat to sse connection

--- lib/personal_mcp_web/controllers/mcp_controller.ex
+++ lib/personal_mcp_web/controllers/mcp_controller.ex
@@ -10,8 +10,20 @@ defmodule PersonalMcpWeb.McpController do
   end
 
   def stream(conn, _params) do
+    conn =
+      conn
+      |> put_resp_header("content-type", "text/event-stream")
+      |> put_resp_header("cache-control", "no-cache")
+      |> put_resp_header("connection", "keep-alive")
+      |> send_chunked(200)
+
+    Stream.repeatedly(fn -> :heartbeat end)
+    |> Stream.each(fn _ ->
+      chunk(conn, ": heartbeat\n\n")
+      Process.sleep(15_000)
+    end)
+    |> Stream.run()
+
     conn
-    |> put_resp_content_type("text/event-stream")
-    |> send_resp(200, "")
   end
 end
